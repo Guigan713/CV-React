e5286af797ddcd7d18acfd2fa9ce1bd915f24f4f
Guigan713 <guillaume.lequin713@gmail.com>
2021-09-14 23:41:48 +0200

    fixed errors

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,16 +1,18 @@
 
 import React from "react"
 
-function Skill({ title, rating }) {
-  return (
+
+function Skill(props) {
+
+    return (
     <div className="skill">
-      <p className="skill__label">{title}</p>
+      <p className="skill__label">{props.title}{props.image}</p>
       <div className="skill__rating">
-        <div className={`circle ${rating > 0 && "circle--plain"}`}></div>
-        <div className={`circle ${rating > 1 && "circle--plain"}`}></div>
-        <div className={`circle ${rating > 2 && "circle--plain"}`}></div>
-        <div className={`circle ${rating > 3 && "circle--plain"}`}></div>
-        <div className={`circle ${rating > 4 && "circle--plain"}`}></div>
+        <div className={`circle ${props.rating > 0 && "circle--plain"}`}></div>
+        <div className={`circle ${props.rating > 1 && "circle--plain"}`}></div>
+        <div className={`circle ${props.rating > 2 && "circle--plain"}`}></div>
+        <div className={`circle ${props.rating > 3 && "circle--plain"}`}></div>
+        <div className={`circle ${props.rating > 4 && "circle--plain"}`}></div>
       </div>
     </div>
   )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,20 +11,21 @@ function Skills() {
       <div className="skills">
         <h2 className="h2">Compétences</h2>
         <img align="left" alt="HTML" src="https://shields.io/badge/-HTML5-E34F26?&logo=HTML5&logoColor=white&style=plastic" />
-        <Skill rating="5" />
+        <Skill title="HTML" rating="5" />
         <img align="left" alt="CSS" src="https://shields.io/badge/-CSS3-1572B6?&logo=CSS3&logoColor=white&style=plastic" />
-        <Skill rating="4" />
+        <Skill title="CSS" rating="4" />
         <img align="left" alt="Javascript" src="https://shields.io/badge/-JavaScript-F7DF1E?&logo=JavaScript&logoColor=white&style=plastic" />
-        <Skill rating="3" />
+        <Skill title="JAVASCRIPT" rating="3" />
         <img align="left" alt="react" src="https://shields.io/badge/-React-61DAFB?&logo=React&logoColor=white&style=plastic" />
-        <Skill rating="4" />
+        <Skill title="REACT" rating="4" />
         <img align="left" alt="nodejs" src="https://img.shields.io/badge/node.js%20-%2343853D.svg?&style=for-the-badge&logo=node.js&logoColor=white&style=plastic" />
-        <Skill rating="3" />
+        <Skill title="NODE.JS" rating="3" />
         <img align="left" alt="MySQL" src="https://shields.io/badge/-MySQL-4479A1?&logo=MySQL&logoColor=white&style=plastic" />
-        <Skill rating="4" />
+        <Skill title="MYSQL" rating="4" />
         <img align="left" alt="npm" src="https://shields.io/badge/-npm-CB3837?&logo=npm&logoColor=white&style=plastic" />
-      <img align="left" alt="Git" src="https://shields.io/badge/-Git-F05032?&logo=Git&logoColor=white&style=plastic" />
-        <Skill rating="4" />
+        <Skill title="NPM"rating="4" /> 
+        {/* <img align="left" alt="Git" src="https://shields.io/badge/-Git-F05032?&logo=Git&logoColor=white&style=plastic" />
+        <Skill title="GIT"rating="4" />  */}
 
       </div>
       <div className="skills">
